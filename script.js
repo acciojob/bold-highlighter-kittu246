93e41c1b431129854f310d36c914c90be3b4f218
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-25 14:02:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+
 function highlight() {
     //Write your code here
 
+    // console.log("hey");
+   
+    let highlight = document.getElementsByTagName("strong");
+   for(let i =0;i<highlight.length;i++){
+    highlight[i].style.color='green';
+   }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+    let highlight = document.getElementsByTagName("strong");
+    for(let i =0;i<highlight.length;i++){
+        highlight[i].style.color='black';
+       }
 }
